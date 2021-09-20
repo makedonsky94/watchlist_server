bb4075f779e7257d8fd421385ff483dfcdfd014a
Alexandr Nevyantsev <anevyantsev@tradingview.com>
2021-09-21 02:22:14 +0300

    Add link to https

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
 require('./repostory/internal/db/connection')
 require('./route/index')
+
+import { app } from './route/express'
+import https from 'https'
+import http from 'http'
+import * as sslConfig from './.ssl.config.json'
+import * as fs from 'fs'
+import * as path from 'path'
+
+
+
+if (sslConfig.ssl) {
+  let options = {
+    key: fs.readFileSync(path.resolve(sslConfig.ssl.key)),
+    cert: fs.readFileSync(path.resolve(sslConfig.ssl.cert)),
+    ca: fs.readFileSync(path.resolve(sslConfig.ssl.ca))
+  }
+
+  const server = https.createServer(options, app)
+  server.listen(443, () => {
+    console.log('listening on *:443');
+  })
+} else {
+  const server = http.createServer(app)
+  server.listen(80, () => {
+    console.log('listening on *:80');
+  })
+}
--- src/route/index.ts
+++ src/route/index.ts
@@ -1,21 +1,14 @@
 import { app } from './express'
-import http from 'http';
-import './../socket';
+import './../socket'
 
 import './rest/watchlist'
 
 
-app.set('views', __dirname + '/../../view');
+app.set('views', __dirname + '/../view');
 app.set('view engine', 'twig');
 
 app.get('/', (req, res) => {
   res.render('index', {
       buttonText : "Send"
-  });
-});
-
-const server = http.createServer(app);
-
-server.listen(80, () => {
-  console.log('listening on *:80');
-});
+  })
+})
